6850ae28f3eb66f6475369ddc53b8fe915d44f54
gigorko1993 <gigorko1993@gmail.com>
2021-08-02 21:07:38 +0300

    add searchbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Searchbar from './Components/Searchbar';
+import s from './App.module.css';
+console.log(s);
+
+class App extends Component {
+  state = {
+    value: '',
+  };
+  setVal(val) {
+    this.setState(prevState => {
+      if (prevState.value === val.toLowerCase()) {
+        alert('The same request! Try something another :)');
+        return;
+      }
+
+      return { value: val.toLowerCase() };
+    });
+  }
+  render() {
+    return (
+      <>
+        <Searchbar onSubmit={this.setVal} />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Searchbar/Searchbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import s from './Searchbar.module.css';
+console.log(s);
+
+class Searchbar extends Component {
+  state = {
+    value: '',
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    if (this.state.value.trim() !== '') {
+      this.props.onSubmit(this.state.value);
+      this.setState({ value: '' });
+    }
+  };
+
+  onChange = e => {
+    this.setState({ value: e.target.value });
+  };
+
+  render() {
+    const { value } = this.state;
+    return (
+      <header className={s.Searchbar}>
+        <form className={s.SearchForm} onSubmit={this.onSubmit}>
+          <button type="submit" className={s.button}>
+            <span className={s.buttonLabel}>Search</span>
+          </button>
+
+          <input
+            onChange={this.onChange}
+            className={s.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={value}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
